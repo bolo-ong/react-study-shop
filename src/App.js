194984b703081daec4b6a6dbe25464f2f32350b7
Lim Chaemin <wldnjscoals@gmail.com>
2022-12-21 20:52:23 +0900

    navigete, nested routes, outlet

--- src/App.js
+++ src/App.js
@@ -1,44 +1,59 @@
-import './App.css';
 import { Navbar, Container, Nav } from 'react-bootstrap';
+import { useState } from 'react';
+import { Routes, Route, Link, useNavigate, Outlet } from 'react-router-dom';
+import './App.css';
 import bg from './img/bg.png';
+import data from './data.js';
+import Detail from './pages/Detail.js';
+import Card from './component/Card.js';
 
 
 function App() {
+
+  let [shoes] = useState(data)
+  let navigate = useNavigate()
+
   return (
     <div className="App">
       <Navbar bg="dark" variant="dark">
         <Container>
           <Navbar.Brand href="#home">Shop</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#cart">Cart</Nav.Link>
+            <Nav.Link onClick={() => { navigate('/') }}>Home</Nav.Link>
+            <Nav.Link onClick={() => { navigate('/detail') }}>Cart</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
 
-      <div className='main-bg' style={{ backgroundImage: `url(${bg})` }}></div>
-      <div className="container">
-        <div className="row">
-          <div className="col-md-4">
-            <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="80%" alt='' />
-            <h4>상품명</h4>
-            <p>상품정보</p>
-          </div>
-          <div className="col-md-4">
-            <img src="https://codingapple1.github.io/shop/shoes2.jpg" width="80%" alt='' />
-            <h4>상품명</h4>
-            <p>상품정보</p>
-          </div>
-          <div className="col-md-4">
-            <img src="https://codingapple1.github.io/shop/shoes3.jpg" width="80%" alt='' />
-            <h4>상품명</h4>
-            <p>상품정보</p>
-          </div>
-        </div>
-      </div>
+      {/* <Link to="/">home</Link>
+      <Link to="detail">상세 페이지</Link> */}
+
+      <Routes>
+        <Route path="/" element={
+          <>
+            <div className='main-bg' style={{ backgroundImage: `url(${bg})` }}></div>
+
+            <div className="container">
+              <div className="row">
+                {shoes.map((data, i) => {
+                  return <Card shoes={data} i={i} />
+                })}
+              </div>
+            </div>
+          </>
+        } />
+        <Route path="/detail" element={<Detail />} />
+        <Route path="*" element={<div>404</div>} />
+
+        <Route path="/event" element={<><h4>오늘의 이벤트</h4><Outlet></Outlet></>}>
+          <Route path="one" element={<p>첫 주문시 양배추즙 서비스</p>} />
+          <Route path="two" element={<p>생일기념 쿠폰받기</p>} />
+        </Route>
+      </Routes>
 
     </div>
   );
 }
+
 
 export default App;
